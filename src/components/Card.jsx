e64d4529dd439fc8fe78f039c68c1afed9b3cc26
Parimal <99127122+ArchProtios@users.noreply.github.com>
2024-03-11 10:48:38 +0530

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <>
+            <div className="card">
+                <img className='img' src="https://picsum.photos/536/354" />
+                <h2>ArchProtios</h2>
+                <p>Hey my name is Parimal Srivastava.</p>
+            </div>
+        </>
+    )
+}
+
+export default Card
